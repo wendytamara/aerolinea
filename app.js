dfe7cf4e029af70d88428a6f8bea2abd5bc6e9a2
wendytamara <anacarlavegam@gmail.com>
2017-11-10 09:21:39 -0500

    avance js

---
+++ app.js
@@ -0,0 +1,40 @@
+// declarar un array que representara los asientos de nuestro avion //con false indicando que estos estan vacios, ocupado = true
+
+var airlineSeats = [
+  false,
+  false,
+  false,
+  false,
+  false,
+  false,
+  false,
+  false,
+  false,
+  false
+];
+
+// contador que nos ayudara a rastrear el numero de asientos //ocupados
+
+var busySeats = 0;
+
+  var paintSeats = function(array){
+    var containerSeats = document.getElementById("seats");
+
+    for (var i = 0; i < var array.length; i++) {
+      var seat = document.createElement("div");
+      seat.ClassName = "seats"{
+
+      //del 1er elemento al 4to va a hacer de 1era clase, del 0 al 3
+          if (i < 4) {
+            seat.style.background = "purple";
+            else {
+              seat.style.background = "yellow";
+            }
+            containerSeats.appendChild(seat);
+        }
+
+      };
+
+paintSeats(airlineSeats);
+    }
+}
