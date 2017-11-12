7ee3b64693432fdd2eb822fc3b1e0886ed4adbf6
wendytamara <anacarlavegam@gmail.com>
2017-11-12 17:13:51 -0500

    avance jss

--- app.js
+++ app.js
@@ -41,7 +41,63 @@ var reserve = function(){
 var chooseZone = function(){
   var choice = prompt("En que zona prefieres reservar \n \n 1. Primera Clase \n 2. Clase Economica \n \n  por favor ingresa el numero de tu preferencia");
 
-    };
+  var checkFirstClassZone  =  function(){
+    var zone = "Primera Clase"
+    for (var i = 0; i < 4; i++) {
+        if (airlineSeats[i]== false) {
+          airlineSeats[i] = true;
+          break;
+        } else if (i == 3 && airlineSeats[i] == true) {
+          reasignEconomicZone(zone);
+        }
+        }
+      };
 
+      var checkEconomicZone = function(){
+        var zone = "Economica";
+        for (var i = 4; i < 10; i++) {
+          if (airlineSeats[i]== false) {
+            airlineSeats[i] = true;
+            break;
+          } else if (i == 9 && airlineSeats[i] == true) {
+             reasignFirstClassZone(zone);
+          }
+        }
+      }
+
+var reasignEconomicZone = function(zone) {
+   var reasing = confirm("ya no quedan mas aientos disponibles en zona" + zone + "  \n   quiere reservar en Primera Clase?");
+   if (reasing == true) {
+     checkEconomicZone();
+   } else {
+     nextFlight();
+   }
+};
+
+var nextFlight = function(){
+  alert("nuestro proximo vuelo as en 3 horas")
+}
+
+var reasignFirstClassZone = function(zone){
+  var reasing = confirm("ya no quedan mas aientos disponibles en " + zone + "  \n   quiere reservar en Primera  Clase?");
+  if (reasing == true) {
+    checkFirstClassZone();
+  }else {
+    nextFlight();
+  }
+}
+
+
+
+if (choice ==1) {
+  checkFirstClassZone();
+} else if (choice ==2){
+  checkEconomicZone();
+} else {
+  alert("por favor ingrese un campo valido")
+}
+//  };
+
+};
 paintSeats(airlineSeats);
 reserve();
